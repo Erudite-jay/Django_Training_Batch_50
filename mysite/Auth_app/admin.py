dc5cafb4c4df9fe189b9ef94c148aab8b83e95d9
Jay <jay@gmail.com>
2024-12-17 14:18:17 -0500

    Day 2 Session 1

--- mysite/Auth_app/admin.py
+++ mysite/Auth_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+admin.site.register(User)
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('id','username', 'age', 'mobile_number', 'email')
+#     search_fields = ('username'),
+#     list_filter = ('age', 'email')
